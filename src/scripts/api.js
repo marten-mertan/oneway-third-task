eadf1a00a2bdf47b3656f9af1beeace514ee83ea
marten-mertan <46864964+marten-mertan@users.noreply.github.com>
2019-07-21 00:56:48 +0300

    Add files via upload

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,1 +1,19 @@
-//что с этим делать - не совсем понятно
+window.API = {
+
+    onFormSubmit: function(params, callbackSuccess, callbackFailed){
+        console.groupCollapsed('onLogin');
+        console.log(params);
+        console.groupEnd();
+
+        setTimeout( function() {
+            var serverResponseSuccess = true;
+
+            if (serverResponseSuccess) {
+                callbackSuccess();
+            } else {
+                callbackFailed();
+            }
+        }, 500)
+    }
+
+}
