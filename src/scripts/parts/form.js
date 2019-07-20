d04b5a8a67e9a990cb18f5b8ace436494f41a1cb
marten-mertan <46864964+marten-mertan@users.noreply.github.com>
2019-07-21 00:57:34 +0300

    Add files via upload

--- src/scripts/parts/form.js
+++ src/scripts/parts/form.js
@@ -1,9 +1,26 @@
 function initForm(){
-    $('#theForm').submit(function() {
-        $('#theForm')[0].style.display = "none";
-        $('.form-send')[0].style.display = "block";
-        return false;
-      });
+
+    $('.js-form').on('submit', e => {
+        e.preventDefault();
+        
+        const $form = $(e.target);
+
+        const params = {
+            formData: $form.serializeArray(),
+        };
+        
+        const callbackSuccess = () => {
+            console.log('Данные успешно отправлены!')
+            $('#theForm')[0].style.display = "none";
+            $('.form-send')[0].style.display = "block";
+        };
+
+        const callbackError = errors => {
+            console.log('Что-то пошло не так!');
+        };
+
+        window.API.onFormSubmit(params, callbackSuccess, callbackError);
+    })
 }
 
 export default initForm;
